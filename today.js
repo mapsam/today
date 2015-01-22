fd547a9c80860a335acee8783e4f589554534eb0
svmatthews <matthews.sam@gmail.com>
2015-01-21 23:37:39 -0800

    T.list()

--- today.js
+++ today.js
@@ -1,18 +1,29 @@
 'use strict';
-window.App = window.App || {};
 
-/*
-**
-**  constructor
-**
-*/
-var TodayConstructor = function(options) {
-  // build scope level paramaters
-  this.options = this.options || {};
+var Today = function(options) {
+  this.options = options || {};
+  this.data = options.data;
+  this.now = new Date();
 }
 
-TodayConstructor.prototype.date = {
-  today: function() {
-    return new Date();
+Today.prototype.list = function() {
+  var list = this.options.id;
+  for (var e = 0; e < this.data.length; e++) {
+    buildDay(this.data[e], list);
+  }
+
+  function buildDay(day, id) {
+    var list = document.getElementById(id),
+        li = document.createElement('li'),
+        d = new Date(day.date),
+        n = new Date();
+    li.innerHTML = day.note;
+    li.className = 'day';
+    if (d == n) {
+      li.className += ' today';
+    }
+    list.appendChild(li);
+
   }
 }
+
